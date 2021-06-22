1195030400c3f44bdd712bdc6f29e8b88a094628
Laurent Caouissin <laurent.caouissin@insee.fr>
2021-06-22 11:48:03 +0200

    Update main.R

--- R-Scripts/main.R
+++ R-Scripts/main.R
@@ -9,16 +9,20 @@ inputFile <- Sys.getenv("INPUT_FILE")
 outputFile <- Sys.getenv("OUTPUT_FILE")
 city <- Sys.getenv("ARGS")
 
-cat("=================================")
-cat("Env variables read in the program")
-cat("=================================")
+print("=================================")
+print("Env variables read in the program")
+print("=================================")
 cat("INPUT_FILE path :", inputFile)
+print("")
 cat("OUTPUT_FILE path:", outputFile)
+print("")
 cat("ARGS :", city)
-cat("=================================")
+print("=================================")
 
+print("Read inputFile")
 df <- read_csv(inputFile)
 # save manipulated data to output folder
 
 # simple copy of input to the output
+print("Write data to the output")
 write_csv(df, outputFile)
